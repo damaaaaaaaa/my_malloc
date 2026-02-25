727f80f5854035b3eb6842da1364ddfa15362425
dama <158841429+damaaaaaaaa@users.noreply.github.com>
2026-02-25 13:38:49 +0100

    modified some printf

--- apps/main.c
+++ apps/main.c
@@ -10,14 +10,14 @@ int main () {
     memcpy (my_char, str, 4 * sizeof (char));
     uint8_t *my_number = (uint8_t*)my_malloc (sizeof (uint8_t));
     *my_number = 129;
-    printf ("%p\n", my_char);
-    printf ("%s\n", my_char);
-    printf ("%p\n", my_number);
-    printf ("%d\n", *my_number);
+    printf ("Pointer at char after my_malloc: %p\n", my_char);
+    printf ("String: %s\n", my_char);
+    printf ("Pointer at integer after my_malloc: %p\n", my_number);
+    printf ("Value: %d\n", *my_number);
 
     uint16_t *my_new_number = (uint16_t*)my_realloc (my_number, sizeof (uint8_t) ,sizeof (uint16_t));
-    printf ("%p\n", my_new_number);
-    printf ("%d\n", *my_new_number);
+    printf ("Pointer at integer after my_realloc: %p\n", my_new_number);
+    printf ("Value: %d\n", *my_new_number);
 
     my_free_block ();
 
@@ -25,10 +25,10 @@ int main () {
     memcpy (my_char2, str, 4 * sizeof (char));
     uint8_t *my_number2 = (uint8_t*)my_malloc (sizeof (uint8_t));
     *my_number2 = 129;
-    printf ("%p\n", my_char2);
-    printf ("%s\n", my_char2);
-    printf ("%p\n", my_number2);
-    printf ("%d\n", *my_number2);
+    printf ("Pointer at char after my_free and my_malloc: %p\n", my_char2);
+    printf ("String: %s\n", my_char2);
+    printf ("Pointer at integer after my_free and my_malloc: %p\n", my_number2);
+    printf ("Value: %d\n", *my_number2);
     my_free_block ();
     return 0;
 }
